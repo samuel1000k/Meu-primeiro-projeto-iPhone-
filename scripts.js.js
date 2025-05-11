b60fb45d3618d6e0c001fe69b923e3cffd10bbec
samuel1000k <124534800+samuel1000k@users.noreply.github.com>
2025-05-11 05:18:56 -0300

    Add files via upload

---
+++ scripts.js.js
@@ -0,0 +1,8 @@
+function trocacor(cor) {
+    let circulo = document.querySelector(".circulo")
+    circulo.style.background = cor
+}
+function trocaimagem(imagem)
+{
+let imgiphone = document.querySelector(".iphone")
+imgiphone.src = imagem}
